b505a430fc3e02a631ed7e29993bb1022d267f82
John <johnzhengyuliu@gmail.com>
2023-07-04 21:26:03 -0500

    basics processing-instructions

---
+++ basics/processing-instructions/tests/processing-instructions.ts
@@ -0,0 +1,37 @@
+import * as anchor from "@coral-xyz/anchor"
+import { Program } from "@coral-xyz/anchor"
+import { ProcessingInstructions } from "../target/types/processing_instructions"
+
+describe("processing-instructions", () => {
+  // Configure the client to use the local cluster.
+  const provider = anchor.AnchorProvider.env()
+  anchor.setProvider(provider)
+
+  const dataAccount = anchor.web3.Keypair.generate()
+  const wallet = provider.wallet
+
+  const program = anchor.workspace
+    .ProcessingInstructions as Program<ProcessingInstructions>
+
+  it("Is initialized!", async () => {
+    // Initialize data account for the program, which is required by Solang
+    const tx = await program.methods
+      .new(wallet.publicKey)
+      .accounts({ dataAccount: dataAccount.publicKey })
+      .signers([dataAccount])
+      .rpc()
+    console.log("Your transaction signature", tx)
+  })
+
+  it("Go to the park!", async () => {
+    await program.methods
+      .goToPark("Jimmy", 3)
+      .accounts({ dataAccount: dataAccount.publicKey })
+      .rpc()
+
+    await program.methods
+      .goToPark("Mary", 10)
+      .accounts({ dataAccount: dataAccount.publicKey })
+      .rpc()
+  })
+})
