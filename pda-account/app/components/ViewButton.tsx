3dc118084378db592b2b9c880ba4644a36186ac2
John <johnzhengyuliu@gmail.com>
2023-07-20 13:38:20 -0500

    manually simulate tx

--- pda-account/app/components/ViewButton.tsx
+++ pda-account/app/components/ViewButton.tsx
@@ -1,6 +1,6 @@
 import { Button } from "@chakra-ui/react"
-import { PublicKey } from "@solana/web3.js"
-import { useWallet } from "@solana/wallet-adapter-react"
+import { PublicKey, TransactionMessage, VersionedTransaction } from "@solana/web3.js"
+import { useConnection, useWallet } from "@solana/wallet-adapter-react"
 import { useProgram } from "@/contexts/ProgramContextProvider"
 // import { program } from "@/utils/anchorSetup"
 
@@ -9,6 +9,7 @@ import { useProgram } from "@/contexts/ProgramContextProvider"
 // It also seems to require prompting the wallet to sign a transaction (even though it's a read-only operation)
 export default function ViewButton() {
   const { publicKey } = useWallet()
+  const {connection} = useConnection()
   const { program } = useProgram()
 
   const onClick = async () => {
@@ -19,12 +20,48 @@ export default function ViewButton() {
       program.programId
     )
 
-    const val = await program.methods
+    const ix = await program.methods
       .get()
       .accounts({ dataAccount: pda })
-      .view()
+      .instruction()
 
-    alert(val)
+    const data = await connection.getLatestBlockhash()
+    
+    const msg = new TransactionMessage({
+      payerKey: publicKey,
+      recentBlockhash: data.blockhash,
+      instructions: [ix]
+    }).compileToV0Message();
+
+    const tx = new VersionedTransaction(msg)
+
+    const txSim = await connection.simulateTransaction(tx)
+    const txSimLogs = txSim.value.logs
+    
+    console.log(txSim)
+    // If value is "0", then returns undefined
+    console.log(txSim.value.returnData?.data[0])
+    // However, return value still shows in program logs
+    console.log(txSim.value.logs)
+
+    // Find the program log that starts with "Program return:"
+    // Then slice out the prefix to get the return data
+    const returnPrefix = `Program return: ${program.programId} `;
+    const returnLog = txSimLogs!.find((log) =>
+      log.startsWith(returnPrefix)
+    );
+    console.log(returnLog)
+    const returnData = returnLog?.slice(returnPrefix.length);
+    console.log(returnData)
+
+    const buffer = Buffer.from(returnData!, 'base64')
+    alert(buffer[0])
+
+
+    // const val = await program.methods
+    //   .get()
+    //   .accounts({ dataAccount: pda })
+    //   .view()
 
     // // Can't use `fetch` because account type not in IDL
     // const val = await program.account.dataAccount.fetch(pda)
